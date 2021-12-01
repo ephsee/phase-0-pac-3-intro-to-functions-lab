a0c14fcc5d898e24a069c5f7c20358d3e6822db1
Francois Spies <fcspies@gmail.com>
2021-12-01 00:57:07 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function shout(string) {
+    return string;
+}
+"hello".toUpperCase();
+function shout(string) {
+    return string.toUpperCase();
+}
+
+
+function whisper(string) {
+    return string;
+}
+"HELLO".toLowerCase();
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+
+function logShout(string) {
+    console.log(string);
+}
+"hello".toUpperCase();
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+
+function logWhisper(string) {
+    console.log(string);
+}
+"HELLO".toLowerCase();
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+
+function sayHiToGrandma(string) {
+    return string;
+}
+
+function sayHiToGrandma(string) {
+    if (string == "hello")
+        return "I can't hear you!";
+    else if (string == "HELLO")
+        return "YES INDEED!";
+    else if (string == "I love you, Grandma.")
+        return "I love you, too.";
+  }
+  
